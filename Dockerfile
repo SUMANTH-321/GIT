fef6155563ae7a3e15cf68c474c579cd17a4fcc5
SUMANTH-321 <104135181+SUMANTH-321@users.noreply.github.com>
2025-07-20 06:26:23 +0000

    MY first commit on clone repo

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
 # Comment
-FROM 
+FROM UBUNTU
--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 // your first java program
 class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!");
+        System.out.println("Hello World!!!!!!!!!!!!!!!!!!!!!!!");
     }
 }
 
